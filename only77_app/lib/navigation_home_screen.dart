dd2efd01aa9d049ef035533c3ae079960e984f46
guangkunxiong <dotnet_xyk@outlook.com>
2023-05-29 15:45:59 +0800

    77:app添加框架

---
+++ only77_app/lib/navigation_home_screen.dart
@@ -0,0 +1,67 @@
+import 'package:only77_app/app_theme.dart';
+import 'package:only77_app/custom_drawer/drawer_user_controller.dart';
+import 'package:only77_app/custom_drawer/home_drawer.dart';
+import 'package:only77_app/help_screen.dart';
+import 'package:only77_app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class NavigationHomeScreen extends StatefulWidget {
+  const NavigationHomeScreen({super.key});
+
+  @override
+  _NavigationHomeScreenState createState() => _NavigationHomeScreenState();
+}
+
+class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
+  Widget? screenView;
+  DrawerIndex? drawerIndex;
+
+  @override
+  void initState() {
+    drawerIndex = DrawerIndex.HOME;
+    screenView = const MyHomePage();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppTheme.white,
+      child: SafeArea(
+        top: false,
+        bottom: false,
+        child: Scaffold(
+          backgroundColor: AppTheme.nearlyWhite,
+          body: DrawerUserController(
+            screenIndex: drawerIndex,
+            drawerWidth: MediaQuery.of(context).size.width * 0.75,
+            onDrawerCall: (DrawerIndex drawerIndexData) {
+              changeIndex(drawerIndexData);
+            },
+            screenView: screenView,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void changeIndex(DrawerIndex drawerIndexdata) {
+    if (drawerIndex != drawerIndexdata) {
+      drawerIndex = drawerIndexdata;
+      switch (drawerIndex) {
+        case DrawerIndex.HOME:
+          setState(() {
+            screenView = const MyHomePage();
+          });
+          break;
+        case DrawerIndex.Help:
+          setState(() {
+            screenView = const HelpScreen();
+          });
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
